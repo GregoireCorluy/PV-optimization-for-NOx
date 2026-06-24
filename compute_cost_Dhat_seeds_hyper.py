e527fe071491fef14e9b3977da2a8a2ceb09bf3f
GregoireCorluy <gcorluy@gmail.com>
2026-06-24 18:33:02 +0200

    set scalings

--- compute_cost_Dhat_seeds_hyper.py
+++ compute_cost_Dhat_seeds_hyper.py
@@ -30,7 +30,7 @@ lists_species_output_QoI = [
     ("linLog", ['H2O2', 'H2O', 'H2', 'HO2', 'N2O', 'NO2', 'NO', 'O2', 'OH', 'logH2O2', 'logH2O', 'logH2', 'logHO2', 'logN2O', 'logNO2', 'logNO', 'logO2', 'logOH']),
     ("log", ['logH2O2', 'logH2O', 'logH2', 'logHO2', 'logN2O', 'logNO2', 'logNO', 'logO2', 'logOH'])
 ]
-list_input_scaling_name = ["None"] #"0to1", "-1to1", "std", "pareto", "mean-pareto"
+list_input_scaling_name = ["0to1", "-1to1", "std", "pareto", "mean-pareto"] #"0to1", "-1to1", "std", "pareto", "mean-pareto"
 list_species_scaling_layer = [True]
 seeds = list(range(nbr_seeds))
 
